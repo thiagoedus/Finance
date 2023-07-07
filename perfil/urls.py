4a5e21ef602665f6d244d6bf737a6e530cd2f9ad
“Thiago <playsd395@gmail.com>
2023-07-07 14:12:52 -0300

    Commit teste

--- perfil/urls.py
+++ perfil/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('gerenciar/', views.gerenciar, name="gerenciar"),
     path('cadastrar_banco', views.cadastrar_banco, name="cadastrar_banco"),
     path('remover_banco/<int:id>', views.remover_banco, name="remover_banco"),
+    path('cadastrar_categoria', views.cadastrar_categoria, name='cadastrar_categoria'),
+    path('deletar_categoria/<int:id>', views.deletar_categoria, name='deletar_categoria')
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -8,18 +8,19 @@ def home(request):
     return render(request, "home.html")
 
 def gerenciar(request):
+    categorias = Categoria.objects.all()
     contas = Conta.objects.all()
     total_contas = 0
     for conta in contas:
         total_contas += conta.valor
-    return render(request, "gerenciar.html", {'contas': contas, 'total': total_contas})
+    return render(request, "gerenciar.html", {'contas': contas, 'total': total_contas, 'categorias': categorias})
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
     banco = request.POST.get('banco')
     tipo = request.POST.get('tipo')
     valor = request.POST.get('valor')
-    icone = request.FILES.get('apelido')
+    icone = request.FILES.get('icone')
 
     if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
         messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
@@ -40,4 +41,24 @@ def cadastrar_banco(request):
 def remover_banco(request, id):
     conta = Conta.objects.filter(id=id)
     conta.delete()
+    messages.add_message(request, constants.SUCCESS, 'Conta deletada com sucesso')
+    return redirect('gerenciar')
+
+def cadastrar_categoria(request):
+    categoria = request.POST.get('categoria')
+    essencial = bool(request.POST.get('essencial'))
+
+    categoria = Categoria(
+        categoria = categoria,
+        essencial = essencial
+    )
+
+    categoria.save()
+    messages.add_message(request, constants.SUCCESS, 'Categoria adicionada com sucesso')
+    return redirect('gerenciar')
+
+def deletar_categoria(request, id):
+    categoria = Categoria.objects.filter(id=id)
+    categoria.delete()
+    messages.add_message(request, constants.SUCCESS, 'Categoria deletada com sucesso')
     return redirect('gerenciar')
